60e637802d1b9f8965e6a499c96bd4a58910fc07
Jiri Korejtko <jiri.korejtko@gmail.com>
2025-03-07 16:08:46 +0100

    Changed logo, added favicons, removed images and support for files

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("src/android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("src/android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
   eleventyConfig.addPassthroughCopy("src/style.css");
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin/config.yml");
